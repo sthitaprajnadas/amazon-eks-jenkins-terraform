a8c92262012baa54064dc7e18264758004baf035
sthitaprajnadas <sthitaprajnadas@gmail.com>
2021-08-16 19:44:24 +0530

    "added dependency"

--- terraform/network.tf
+++ terraform/network.tf
@@ -105,9 +105,12 @@ resource "aws_nat_gateway" "nat-gw" {
   depends_on = [aws_eip.elastic-ip-for-nat-gw]
 }
 resource "aws_route" "nat-gw-route" {
+
+  depends_on = [aws_route_table.private-route-table]  # introduced this dependency as tf apply was throwing error
   route_table_id         = "${aws_route_table.private-route-table.id}"
   nat_gateway_id         = "${aws_nat_gateway.nat-gw.id}"
   destination_cidr_block = "0.0.0.0/0"
+
 }
 resource "aws_internet_gateway" "development-igw" {
   vpc_id = "${aws_vpc.development-vpc.id}"
